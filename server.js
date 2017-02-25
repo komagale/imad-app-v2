f1f9462e1165a90bf9d6e363df2683263de5be17
komagale <ekg@vcet.ac.in>
2017-02-25 16:47:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/article-one', function (req, res) {
+  res.send('Árticle one requested and will be served here');
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/article-two', function (req, res) {
+  res.send('Árticle one requested and will be served here');
 });
 
 
